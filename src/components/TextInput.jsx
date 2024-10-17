14063f84e38186bc65ba1e9421320b521b5a885b
akash-sagar-1 <akash.sagar@ranium.in>
2024-10-17 18:42:23 +0530

    added validation and toast message

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -4,34 +4,45 @@ import { useToast } from '@chakra-ui/react';
 
 const TextInput = () => {
     const [text, setText] = useState('');
-  
+    const toast = useToast();
+
     const submitText = () => {
-      console.log('submit');
+        if (text === '') {
+            toast({
+                title: 'Text field is empty.',
+                description: 'Please enter some text to extract keywords.',
+                status: 'error',
+                duration: 5000,
+                isClosable: false,
+            });
+            return;
+        }
+        console.log('proceed');
     };
-  
+
     return (
-      <>
-        <Textarea
-          bg='blue.400'
-          padding={4}
-          marginTop={6}
-          height={200}
-          color='white'
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-  
-        <Button
-          bg='blue.500'
-          color='white'
-          marginTop={4}
-          width='100%'
-          _hover={{ bg: 'blue.700' }}
-          onClick={submitText}
-        >
-          Extract Keywords
-        </Button>
-      </>
+        <>
+            <Textarea
+                bg='blue.400'
+                padding={4}
+                marginTop={6}
+                height={200}
+                color='white'
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+
+            <Button
+                bg='blue.500'
+                color='white'
+                marginTop={4}
+                width='100%'
+                _hover={{ bg: 'blue.700' }}
+                onClick={submitText}
+            >
+                Extract Keywords
+            </Button>
+        </>
     );
-  };
-  export default TextInput;
+};
+export default TextInput;

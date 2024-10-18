69bef4d38b3ba12b9a99ca4837cdcb831aeef582
akash-sagar-1 <akash.sagar@ranium.in>
2024-10-18 11:18:40 +0530

    added open api function to call api end point

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,51 @@ import { useState } from 'react';
 
 const App = () => {
 
-  const extractKeywords = (text) => {
-    console.log(text);
+  const [keywords, setKeywords] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const extractKeywords = async (text) => {
+    setLoading(true);
+    setIsOpen(true);
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: 'gpt-3.5-turbo-instruct',
+        prompt:
+          'Extract keywords from this text. Make the first letter of every word uppercase and separate with commas:\n\n' +
+          text +
+          '',
+        temperature: 0.5,
+        max_tokens: 60,
+        top_p: 1.0,
+        frequency_penalty: 0.8,
+        presence_penalty: 0.0,
+      }),
+    };
+
+    try {
+      const response = await fetch(import.meta.env.VITE_OPENAI_API_URL, options);
+      const json = await response.json();
+      console.log(json.choices[0].text.trim());
+      setKeywords(json.choices[0].text.trim());
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
   };
+
 
   return (
     <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
       <Container maxW='3xl' centerContent>
         <Header />
-        <TextInput extractKeywords={extractKeywords}/>
+        <TextInput extractKeywords={extractKeywords} />
         <Footer />
       </Container>
     </Box>
